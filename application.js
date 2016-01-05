edf5910d4bd0b49df8a83d2f2e870167215861eb
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-05 11:48:29 -0500

    Retail Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -1,7 +1,9 @@
 /*Created 2015-02-28 by CodeCloud Team*/
 
 
-$(".scroll").click(function(event) {
-    event.preventDefault();
-    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
-} );
+$(document).ready(){
+    $(".scroll").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
+    } );
+})
