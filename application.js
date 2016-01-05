df65df589ddf757821b6850046f2054a8c9a1a0f
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-05 11:46:21 -0500

    Retail Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -4,3 +4,7 @@ function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
+
+$("#link").click(function() {
+   scrollToAnchor('id3');
+});
