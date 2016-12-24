0da1b44a1a1434ec074e14a3499fce3932db8fdb
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-12-24 00:17:27 -0500

    Retail Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -88,6 +88,11 @@ function scroll(){
 function loadStores(callback){
     if(typeof(sessionStorage.mallData) == 'undefined'){
         sessionStorage.setItem('mallData', JSON.stringify(data));
+        $.getJSON(sessionStorage.MM_URL).done(function(data) {
+            sessionStorage.setItem('mallData', JSON.stringify(data));
+            log('done fetching stores from: '+sessionStorage.MM_URL);
+            callback();
+        });
     }
     else{
         callback();
