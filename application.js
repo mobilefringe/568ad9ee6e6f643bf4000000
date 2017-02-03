2bd80eb2c3940b27ed81d112b0d52b652925bd09
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-02-03 10:27:35 -0500

    Retail Maverick 1.1  /application.js

--- application.js
+++ application.js
@@ -32,7 +32,7 @@ function init(){
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window >= bottom_of_object ){
+            if( bottom_of_window > bottom_of_object ){
                 
                 $(this).animate({'opacity':'1'},500);
                     
