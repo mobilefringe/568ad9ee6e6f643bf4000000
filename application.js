469099e40738d1f56e19e6929fc23f8bd5c8af05
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-12 11:03:30 -0500

    Retail Maverick 1.3  /application.js

--- application.js
+++ application.js
@@ -164,19 +164,7 @@ function renderTestimony(container, template, collection){
     });
     $(container).html(item_rendered.join(''));
 }
-// function renderSingleTestimony(container, template, collection){
-//     var item_list = [];
-//     var item_rendered = [];
-//     var template_html = $(template).html();
-//     Mustache.parse(template_html); 
-//     // $.each( collection , function( key, val ) {
-//         var repo_rendered = Mustache.render(template_html,collection);
-//         item_rendered.push(repo_rendered);
-       
-//     // });
-//     $(container).html(item_rendered.join(''));
-// }
-function renderSingleTestimony(container, template, val){
+function renderSingleItem(container, template, val){
     var item_list = [];
     var item_rendered = [];
     var template_html = $(template).html();
@@ -184,6 +172,19 @@ function renderSingleTestimony(container, template, val){
     // $.each( collection , function( key, val ) {
         console.log("renderGeneral",val)
         var repo_rendered = Mustache.render(template_html,val.posts[0]);
+        item_rendered.push(repo_rendered);
+    // });
+    $(container).html(item_rendered.join(''));
+}
+
+function renderSingleItem(container, template, val){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    // $.each( collection , function( key, val ) {
+        console.log("renderGeneral",val)
+        var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
     // });
     $(container).html(item_rendered.join(''));
