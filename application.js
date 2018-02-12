1c85274e78693f9ae25394e79451bb18e6a28414
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-12 10:59:29 -0500

    Retail Maverick 1.3  /application.js

--- application.js
+++ application.js
@@ -170,7 +170,7 @@ function renderSingleTestimony(container, template, collection){
     var template_html = $(template).html();
     Mustache.parse(template_html); 
     $.each( collection , function( key, val ) {
-        var repo_rendered = Mustache.render(template_html,val.posts);
+        var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
        
     });
